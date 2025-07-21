b5a5ac43c606a6062138d724a9ecb62c6086df82
Anusuya-SK <anusuyak98.Ak@gmail.com>
2025-07-21 20:22:09 +0530

    Find-UniqueNumber

--- Code-Snippets/UniqueNumber.js
+++ Code-Snippets/UniqueNumber.js
@@ -1,2 +1,11 @@
 // Find the first unique number in an array
 // Input: [1,2,2,3,4,3] -> Output: 1
+
+const uniqueNum = arr => {
+    return arr.find(item => {
+        return arr.filter(x => x === item).length == 1;
+    })
+}
+console.log(uniqueNum([1,2,2,3,4,3]));
+
+//Same as Non-Repeating
